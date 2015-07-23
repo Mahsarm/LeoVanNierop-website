25bd3dea2bd8bf15a60f963380453a4f2e670163
Mahsa Rahimi <mahsa.84rm@gmail.com>
2015-07-23 13:54:20 -0400

    Delete costume-js.js

--- js/costume-js.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $('#accordion').on('shown.bs.collapse', function (e) {
-        var offset = $('.panel.panel-default > .panel-collapse.in').offset();
-            if(offset)$('html,body').scrollTop(offset.top);
-    });
-});
